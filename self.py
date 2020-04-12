600d7a1d32fca53f62233ff42e0f7f803827a075
Tarun M Krishna <43349589+taruntmk2712@users.noreply.github.com>
2020-04-12 20:22:31 +0530

    Update self.py

--- self.py
+++ self.py
@@ -5,29 +5,36 @@ from tkinter import font
 def show_data():
     movieName = ent.get()
 
-
     df = pd.read_csv("C:\\Users\\Tarun M Krishna\\Documents\\6th SEM\\AI\\ratings.csv") 
     df.head() 
-
+    
+    # Check out all the movies and their respective IDs 
     movie_titles = pd.read_csv("C:\\Users\\Tarun M Krishna\\Documents\\6th SEM\\AI\\movies.csv") 
     movie_titles.head() 
     data = pd.merge(df, movie_titles, on='movieId',how='left') 
     data.head() 
-
+    
+    # Calculate mean rating of all movies
     data.groupby('title')['rating'].mean().sort_values(ascending=False).head() 
+    
+    # creating dataframe with 'rating' count values
     ratings = pd.DataFrame(data.groupby('title')['rating'].mean()) 
     ratings['Total Ratings'] = pd.DataFrame(data.groupby('title')['rating'].count()) 
     ratings.head()
-
+    
+    
+    # Sorting values according to 
+    # the 'num of rating column'
     moviemat = data.pivot_table(index ='userId',columns ='title', values ='rating') 
     moviemat.head() 
 
     ratings.sort_values('Total Ratings', ascending = False).head(10) 
 
-
+    # analysing correlation with similar movies
     movie_user_ratings = moviemat[movieName]
     movie_user_ratings.head() 
-
+    
+    # analysing correlation with similar movies 
     similar_to_userinput = moviemat.corrwith(movie_user_ratings) 
 
     corr_userinput = pd.DataFrame(similar_to_userinput, columns =['Correlation']) 
@@ -35,19 +42,14 @@ def show_data():
 
     corr_userinput.head()
 
+    # Similar movies like input
     corr_userinput.sort_values('Correlation', ascending = False).head(10) 
     corr_userinput = corr_userinput.join(ratings['Total Ratings'])
     corr_userinput.head()
 
-    
-    '''res = corr_userinput[corr_userinput['Total Ratings']>100].sort_values('Correlation', ascending = False).head()
-    res = res.merge(movie_titles,on='title',how='left')
-    res.head(10)'''
     res=[]
     res.append(corr_userinput[corr_userinput['Total Ratings']>100].sort_values('Correlation', ascending = False).merge(movie_titles,on='title',how='outer').head())
-    '''res1=[]
-    res1.append(res)
-    res2=""'''
+   
     res2=""
     for i in res:
         res2+=str(i)
